c7fc69c836e1fb6dd42d1db6852fdeb44ad1c9aa
StefanJordanovMemec <sjordanov080807@gmail.com>
2026-01-17 18:05:00 +0100

    Update settings.py for Cloudinary + production deploy

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -124,8 +124,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/videos/'
