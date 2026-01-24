3ca941233dc4c1ed63d6f39cad7a5023ac5f7860
StefanJordanovMemec <sjordanov080807@gmail.com>
2026-01-24 15:22:27 +0100

    Add professional login page with background image

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -106,7 +106,9 @@ USE_TZ = True
 # -------------------------------
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # -------------------------------
 # Media files (Cloudinary)
 # -------------------------------
